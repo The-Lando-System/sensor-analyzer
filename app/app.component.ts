41e7656583821f63826033f0341ec1bd8a1f800d
Matt <matt.voget@gmail.com>
2017-04-23 14:18:36 -0600

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  moduleId: module.id,
+  selector: 'sensor-app',
+  templateUrl: 'app.component.html',
+  styleUrls: [ 'app.component.css' ],
+  providers: []
+})
+export class AppComponent {
+
+  welcome = 'Sensor Analyzer';
+
+}
